e888789475f3ecd17f2f1082e735278b5bd48951
lknknm <108239558+lknknm@users.noreply.github.com>
2024-05-07 10:20:19 -0300

    Implementation of initial sphere

--- OttocentoRenderer/Renderer.cpp
+++ OttocentoRenderer/Renderer.cpp
@@ -27,6 +27,7 @@ void Renderer::Render()
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight()};
+			coord = coord * 2.0f - 1.0f; // Remapping -1 -> 1
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
 		}
 	}
@@ -38,5 +39,28 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
-	return 0xff000000 | (g << 8) | r;
+
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	float radius = 0.5f;
+	// rayDirection = glm::normalize(rayDirection);
+
+	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+	// where
+	// a = ray origin
+	// b = ray direction
+	// r = radius
+	// t = hit distance
+
+	// in a quadratic equation: ax² + bx + c = 0:
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	// in a quadratic formula discriminant b² - 4ac:
+	float discriminant = b * b - 4.0f * a * c;
+
+	if (discriminant >= 0.0f)
+		return 0xffff00ff;
+	return 0xff000000;
 }
